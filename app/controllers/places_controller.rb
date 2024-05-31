c9868c95f33de71a56f1ba98be91dc811e01b343
Daniel Tsypkin <daniel.tsypkin@kellogg.northwestern.edu>
2024-05-31 20:06:50 +0000

    Added places page to home

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,18 +5,19 @@ class PlacesController < ApplicationController
   end
   
   def home
-    @user = User.find_by({ "id" => session["user_id"] })
-    if @user != nil
-      redirect_to "/places"
-    else
-      redirect_to "/login"
-    end
+
+    redirect_to "/places"
   end
 
   def show
     @user = User.find_by({ "id" => session["user_id"] })
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @user["id"]})
+    if @user != nil
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @user["id"]})
+    else
+      flash["notice"] = "Please login."
+      redirect_to "/login"
+    end
   end
 
   def new
