d8aeeca81a8eb33ed9ca6845f4cd533a7818d391
Daniel Tsypkin <daniel.tsypkin@kellogg.northwestern.edu>
2024-05-31 19:26:52 +0000

    Done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,8 +9,18 @@ class EntriesController < ApplicationController
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+  
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
   end
 
 end
